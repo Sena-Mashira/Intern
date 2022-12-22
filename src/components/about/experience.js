43d4047379b0c6697cd8215722100109193bdf1f
Anand Hari Krsna <anandhkrsna@gmail.com>
2022-12-22 13:32:00 +0800

    update build for gh-pages

--- src/components/about/experience.js
+++ src/components/about/experience.js
@@ -13,7 +13,9 @@ function Experience() {
                 <h4>Graphic Designer - Freelance</h4>
                 <ul>
                     <li>Created design of product package and social media content for incense products on Facebook and Instagram</li>
+                    <br></br>
                     <li>Documented all activities related to organizational event.</li>
+                    <br></br>
                     <li>
                         Participated in making incense from the beginning to the end of the process (printed the design package and
                         product packaging stage).
--- src/components/about/project.js
+++ src/components/about/project.js
@@ -5,7 +5,7 @@ function Project() {
                 <h2>.Project</h2>
                 <h3>Fyve Indonesia</h3>
                 <p>This website used for the sale and catalog of electronic products such as CPUs, keyboards, mice, etc.</p>
-                <button><a href="https://sena-mashira.github.io/fyve/">visit site</a></button>
+                <button><a href="https://sena-mashira.github.io/fyve/" target="_blank">visit site</a></button>
             </span>
         </div>
     )
